fa3ab27ad1637a96e04398ed193738578d09629c
Quan <61860115+Dracozure@users.noreply.github.com>
2023-09-17 18:31:59 -0700

    Fix eventListener bug
    addEventListener, once added to an element is not removed until
    element is replaced or explicitly removed. If more addEventListeners
    are added on top of already existing event listeners on element, each
    event will now register multiple times equal to the number of listeners
    existing on that element, even if the event only triggered once.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let currentGameState;
+let currentGameState = 'start';
 const gameStateID = '#display-choices';
 const originalGameState = document.querySelector(gameStateID);
-const cloneOriginalGameState = originalGameState.cloneNode(true);
 let playerRockPaperScissorsButtons = document.querySelectorAll('button');
 
 toggleGameState(gameStateID, 'start');
 
 function toggleGameState(elementToggleID, targetGameState) {
     const elementToggle = document.querySelector(elementToggleID);
-
     switch (targetGameState) {
         case 'start':
             elementToggle.replaceWith(createDisplayButton('start'));
@@ -16,63 +14,59 @@ function toggleGameState(elementToggleID, targetGameState) {
             playerRockPaperScissorsButtons = document.querySelectorAll('button');
             break;
         case 'restart':
+            originalGameState.replaceWith(originalGameState.cloneNode(true));
             elementToggle.replaceWith(createDisplayButton('restart'));
             currentGameState = 'restart';
             playerRockPaperScissorsButtons = document.querySelectorAll('button');
             break;
         case 'game':
-            elementToggle.replaceWith(cloneOriginalGameState);
+            elementToggle.replaceWith(originalGameState.cloneNode(true));
             currentGameState = 'game';
             playerRockPaperScissorsButtons = document.querySelectorAll('button');
             break;
-        case 'player win':
-            const playerWinAnnouncement = document.createElement('h1');
-
-            playerWinAnnouncement.textContent = 'PLAYER WINS!';
-            playerWinAnnouncement.classList.add('winner-announcement')
-
-            elementToggle.parentNode.insertBefore(playerWinAnnouncement, elementToggle.parentNode.firstChild);
-            break;
-        case 'computer win':
-            const computerWinAnnouncement = document.createElement('h1');
-
-            computerWinAnnouncement.textContent = 'COMPUTER WINS!';
-            playerWinAnnouncement.classList.add('winner-announcement')
-
-            elementToggle.parentNode.insertBefore(computerWinAnnouncement, elementToggle.parentNode.firstChild);
-            break;
-        case 'tie':
-            const tieWinAnnouncement = document.createElement('h1');
-
-            tieWinAnnouncement.textContent = 'IT\'S A TIE!';
-            playerWinAnnouncement.classList.add('winner-announcement')
-
-            elementToggle.parentNode.insertBefore(tieWinAnnouncement, elementToggle.parentNode.firstChild);
-            break;
     }
 
-    playerRockPaperScissorsButtons.forEach((button) => {
-        button.addEventListener('click', () => {
+    addEventListener(playerRockPaperScissorsButtons, currentGameState);
+}
+
+function addEventListener(nodeList, currentGameState) {
+    nodeList.forEach((element) => {
+        element.addEventListener('click', () => {
             if (currentGameState === 'start') {
                 toggleGameState(gameStateID, 'game');
             } else if (currentGameState === 'game') {
-                switch (button.id) {
-                    case 'rock':
-                        console.log(playRockPaperScissors('rock', getComputerChoice()));
-                        break;
-                    case 'paper':
-                        console.log(playRockPaperScissors('paper', getComputerChoice()));
-                        break;
-                    case 'scissors':
-                        console.log(playRockPaperScissors('scissors', getComputerChoice()));
-                        break;
+                if (element.id === 'rock') {
+                    playRockPaperScissors('rock', getComputerChoice());
+                } else if (element.id === 'paper') {
+                    playRockPaperScissors('paper', getComputerChoice());
+                } else {
+                    playRockPaperScissors('scissors', getComputerChoice());
                 }
-            };
+            } else {
+                const announcement = document.querySelector('.winner-announcement');
+
+                resetDisplay();
+                announcement.remove();
+                toggleGameState(gameStateID, 'start');
+            }
         });
     });
 }
 
+function resetDisplay() {
+    const playerScore = document.getElementById('player-score');
+    const computerScore = document.getElementById('computer-score');
+    const playerChoice = document.getElementById('player-choice');
+    const computerChoice = document.getElementById('computer-choice');
+
+    playerScore.textContent = 'Score: 0';
+    computerScore.textContent = 'Score: 0';
+    playerChoice.textContent = 'CHOICE';
+    computerChoice.textContent = 'CHOICE';
+}
+
 function createDisplayButton(textContent) {
+
     const gameStatebutton = document.createElement('button');
 
     gameStatebutton.textContent = textContent.toUpperCase();
@@ -99,6 +93,9 @@ function getComputerChoice() {
 }
 
 function playRockPaperScissors(playerSelection, computerSelection) {
+
+    console.log("PLAYGAME");
+
     const playerChoice = document.getElementById('player-choice');
     const computerChoice = document.getElementById('computer-choice');
     const playerScore = document.getElementById('player-score');
@@ -135,17 +132,48 @@ function playRockPaperScissors(playerSelection, computerSelection) {
         addOnePoint(computerScore);
     }
 
+    if (getWinner(playerScore, computerScore) === 'player win') {
+        announceWinner('player win');
+        toggleGameState(gameStateID, 'restart');
+    } else if (getWinner(playerScore, computerScore) === 'computer win') {
+        announceWinner('computer win');
+        toggleGameState(gameStateID, 'restart');
+    } else if (getWinner(playerScore, computerScore) === 'tie'){
+        announceWinner('tie');
+        toggleGameState(gameStateID, 'restart');
+    }
+}
+
+function getWinner(playerScore, computerScore) {
     if (playerScore.textContent.slice(-1) == '5' 
     || computerScore.textContent.slice(-1) == '5') {
         if (playerScore.textContent.slice(-1) > computerScore.textContent.slice(-1)) {
-            toggleGameState(gameStateID, 'player win');
+            return 'player win';
         } else if (playerScore.textContent.slice(-1) < computerScore.textContent.slice(-1)) {
-            toggleGameState(gameStateID, 'computer win');
-        } else {
-            toggleGameState(gameStateID, 'tie');
+            return 'computer win';
+        } else if (playerScore.textContent.slice(-1) === computerScore.textContent.slice(-1)){
+            return 'tie';
         }
-
-        toggleGameState(gameStateID, 'restart');
     }
+}
+
+function announceWinner(winner) {
+    const announcement = document.createElement('h1');
+    announcement.classList.add('winner-announcement');
+    const elementToggle = document.querySelector(gameStateID);
+
+    switch (winner) {
+        case 'player win':
+            announcement.textContent = 'PLAYER WINS!';
+            break;
+        case 'computer win':
+            announcement.textContent = 'COMPUTER WINS!';
+            break;
+        case 'tie':
+            announcement.textContent = 'IT\'S A TIE!';
+            break;
+    }
+
+    elementToggle.parentNode.insertBefore(announcement, elementToggle.parentNode.firstChild);
 }
 
